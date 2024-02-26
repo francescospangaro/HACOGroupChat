c4e15e98fa3a206e55d780b56bf06434d036c0c6
jackorse <giacomo.orsenigo@gmail.com>
2024-02-26 15:05:45 +0100

    Re-add deleted test :cactus:

--- main/src/test/java/org/HACO/VCTest.java
+++ main/src/test/java/org/HACO/VCTest.java
@@ -89,6 +89,45 @@ class VCTest {
     }
 
     @Test
+    void sendMsg() throws ExecutionException, InterruptedException, TimeoutException {
+        CompletableFuture<String> c1Promise = new CompletableFuture<>();
+        CompletableFuture<String> c2Promise = new CompletableFuture<>();
+        CompletableFuture<ChatRoom> chat1Promise = new CompletableFuture<>();
+        CompletableFuture<ChatRoom> chat2Promise = new CompletableFuture<>();
+        CompletableFuture<Message> msg1Promise = new CompletableFuture<>();
+        CompletableFuture<Message> msg2Promise = new CompletableFuture<>();
+        Client c1 = new Client("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
+                e -> c1Promise.complete((String) e.getNewValue()),
+                e -> msg1Promise.complete((Message) e.getNewValue()));
+        Client c2 = new Client("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
+                e -> c2Promise.complete((String) e.getNewValue()),
+                e -> msg2Promise.complete((Message) e.getNewValue()));
+
+        System.out.println(c1Promise.get(500, TimeUnit.MILLISECONDS));
+        System.out.println(c2Promise.get(500, TimeUnit.MILLISECONDS));
+
+        Set<String> users = Set.of("id1", "id2");
+        c1.createRoom("room", users);
+
+        ChatRoom chat = chat1Promise.get(500, TimeUnit.MILLISECONDS);
+        chat2Promise.get(500, TimeUnit.MILLISECONDS);
+
+        c1.sendMessage("TEST", chat, 0);
+
+        Message m1 = msg1Promise.get(500, TimeUnit.MILLISECONDS);
+        Message m2 = msg2Promise.get(1000, TimeUnit.MILLISECONDS);
+
+        assertEquals("TEST", m1.msg());
+        assertEquals("id1", m1.sender());
+
+        assertEquals("TEST", m2.msg());
+        assertEquals("id1", m2.sender());
+
+        c1.disconnect();
+        c2.disconnect();
+    }
+
+    @Test
     void vcTest() throws ExecutionException, InterruptedException, TimeoutException {
         CompletableFuture<String> c1Promise = new CompletableFuture<>();
         CompletableFuture<String> c2Promise = new CompletableFuture<>();
