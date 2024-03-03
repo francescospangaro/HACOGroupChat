b95eabeb8b58720c0c2f8251da8c4e3e7f89c405
jackorse <giacomo.orsenigo@gmail.com>
2024-03-03 18:47:35 +0100

    Use try with resources to close stuff even if a test fails

--- main/src/test/java/org/HACO/ChatTest.java
+++ main/src/test/java/org/HACO/ChatTest.java
@@ -33,22 +33,21 @@ class ChatTest {
     void connect() throws ExecutionException, InterruptedException, TimeoutException {
         CompletableFuture<String> c1Promise = new CompletableFuture<>();
         CompletableFuture<String> c2Promise = new CompletableFuture<>();
-        Peer c1 = new Peer("id1", 12345, e -> {
-        }, e -> c1Promise.complete((String) e.getNewValue()), e -> {
-        }, true);
 
-        Peer c2 = new Peer("id2", 12346, e -> {
-        }, e -> c2Promise.complete((String) e.getNewValue()), e -> {
-        }, true);
+        try (
+                Peer c1 = new Peer("id1", 12345, e -> {
+                }, e -> c1Promise.complete((String) e.getNewValue()), e -> {
+                }, true);
+                Peer c2 = new Peer("id2", 12346, e -> {
+                }, e -> c2Promise.complete((String) e.getNewValue()), e -> {
+                }, true)
+        ) {
+            String u1 = c1Promise.get(500, TimeUnit.MILLISECONDS);
+            String u2 = c2Promise.get(500, TimeUnit.MILLISECONDS);
 
-        String u1 = c1Promise.get(500, TimeUnit.MILLISECONDS);
-        String u2 = c2Promise.get(500, TimeUnit.MILLISECONDS);
-
-        assertEquals("id2", u1);
-        assertEquals("id1", u2);
-
-        c1.close();
-        c2.close();
+            assertEquals("id2", u1);
+            assertEquals("id1", u2);
+        }
     }
 
     @Test
@@ -57,29 +56,29 @@ class ChatTest {
         CompletableFuture<String> c2Promise = new CompletableFuture<>();
         CompletableFuture<ChatRoom> chat1Promise = new CompletableFuture<>();
         CompletableFuture<ChatRoom> chat2Promise = new CompletableFuture<>();
-        Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
-                e -> c1Promise.complete((String) e.getNewValue()), e -> {
-        }, true);
-        Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
-                e -> c2Promise.complete((String) e.getNewValue()), e -> {
-        }, true);
-        c1Promise.get(500, TimeUnit.MILLISECONDS);
-        c2Promise.get(500, TimeUnit.MILLISECONDS);
+        try (
+                Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> c1Promise.complete((String) e.getNewValue()), e -> {
+                }, true);
+                Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> c2Promise.complete((String) e.getNewValue()), e -> {
+                }, true)
+        ) {
+            c1Promise.get(500, TimeUnit.MILLISECONDS);
+            c2Promise.get(500, TimeUnit.MILLISECONDS);
 
-        Set<String> users = Set.of("id1", "id2");
-        c1.createRoom("room", users);
+            Set<String> users = Set.of("id1", "id2");
+            c1.createRoom("room", users);
 
-        ChatRoom chat1 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
-        ChatRoom chat2 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
+            ChatRoom chat1 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
+            ChatRoom chat2 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
 
-        assertEquals(users, chat1.getUsers());
-        assertEquals("room", chat1.getName());
+            assertEquals(users, chat1.getUsers());
+            assertEquals("room", chat1.getName());
 
-        assertEquals(users, chat2.getUsers());
-        assertEquals("room", chat2.getName());
-
-        c1.close();
-        c2.close();
+            assertEquals(users, chat2.getUsers());
+            assertEquals("room", chat2.getName());
+        }
     }
 
     @Test
@@ -90,36 +89,36 @@ class ChatTest {
         CompletableFuture<ChatRoom> chat2Promise = new CompletableFuture<>();
         CompletableFuture<Message> msg1Promise = new CompletableFuture<>();
         CompletableFuture<Message> msg2Promise = new CompletableFuture<>();
-        Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
-                e -> c1Promise.complete((String) e.getNewValue()),
-                e -> msg1Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
+        try (
+                Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> c1Promise.complete((String) e.getNewValue()),
+                        e -> msg1Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
 
-        Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
-                e -> c2Promise.complete((String) e.getNewValue()),
-                e -> msg2Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
+                Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> c2Promise.complete((String) e.getNewValue()),
+                        e -> msg2Promise.complete(((MessageGUI) e.getNewValue()).message()), true)
+        ) {
+            System.out.println(c1Promise.get(500, TimeUnit.MILLISECONDS));
+            System.out.println(c2Promise.get(500, TimeUnit.MILLISECONDS));
 
-        System.out.println(c1Promise.get(500, TimeUnit.MILLISECONDS));
-        System.out.println(c2Promise.get(500, TimeUnit.MILLISECONDS));
+            Set<String> users = Set.of("id1", "id2");
+            c1.createRoom("room", users);
 
-        Set<String> users = Set.of("id1", "id2");
-        c1.createRoom("room", users);
+            ChatRoom chat = chat1Promise.get(500, TimeUnit.MILLISECONDS);
+            chat2Promise.get(500, TimeUnit.MILLISECONDS);
 
-        ChatRoom chat = chat1Promise.get(500, TimeUnit.MILLISECONDS);
-        chat2Promise.get(500, TimeUnit.MILLISECONDS);
+            c1.sendMessage("TEST", chat, 0);
 
-        c1.sendMessage("TEST", chat, 0);
+            Message m1 = msg1Promise.get(500, TimeUnit.MILLISECONDS);
+            Message m2 = msg2Promise.get(1000, TimeUnit.MILLISECONDS);
 
-        Message m1 = msg1Promise.get(500, TimeUnit.MILLISECONDS);
-        Message m2 = msg2Promise.get(1000, TimeUnit.MILLISECONDS);
+            assertEquals("TEST", m1.msg());
+            assertEquals("id1", m1.sender());
 
-        assertEquals("TEST", m1.msg());
-        assertEquals("id1", m1.sender());
+            assertEquals("TEST", m2.msg());
+            assertEquals("id1", m2.sender());
 
-        assertEquals("TEST", m2.msg());
-        assertEquals("id1", m2.sender());
-
-        c1.close();
-        c2.close();
+        }
     }
 
     @Test
@@ -134,66 +133,65 @@ class ChatTest {
         CountDownLatch users1 = new CountDownLatch(2);
         CountDownLatch users2 = new CountDownLatch(2);
         CountDownLatch users3 = new CountDownLatch(2);
-        Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
-                e -> users1.countDown(),
-                e -> msg1Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
+        try (
+                Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> users1.countDown(),
+                        e -> msg1Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
 
-        Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
-                e -> users2.countDown(),
-                e -> msg2Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
+                Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> users2.countDown(),
+                        e -> msg2Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
 
-        Peer c3 = new Peer("id3", 12347, e -> chat3Promise.complete((ChatRoom) e.getNewValue()),
-                e -> users3.countDown(),
-                e -> {
-                    msg3List.add(((MessageGUI) e.getNewValue()).message());
-                    msg3.countDown();
-                }, true);
-        assertTrue(users1.await(500, TimeUnit.MILLISECONDS));
-        assertTrue(users2.await(500, TimeUnit.MILLISECONDS));
-        assertTrue(users3.await(500, TimeUnit.MILLISECONDS));
+                Peer c3 = new Peer("id3", 12347, e -> chat3Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> users3.countDown(),
+                        e -> {
+                            msg3List.add(((MessageGUI) e.getNewValue()).message());
+                            msg3.countDown();
+                        }, true)
+        ) {
+            assertTrue(users1.await(500, TimeUnit.MILLISECONDS));
+            assertTrue(users2.await(500, TimeUnit.MILLISECONDS));
+            assertTrue(users3.await(500, TimeUnit.MILLISECONDS));
 
-        Set<String> users = Set.of("id1", "id2", "id3");
-        c1.createRoom("room", users);
+            Set<String> users = Set.of("id1", "id2", "id3");
+            c1.createRoom("room", users);
 
-        ChatRoom chat1 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
-        ChatRoom chat2 = chat2Promise.get(500, TimeUnit.MILLISECONDS);
-        ChatRoom chat3 = chat3Promise.get(500, TimeUnit.MILLISECONDS);
+            ChatRoom chat1 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
+            ChatRoom chat2 = chat2Promise.get(500, TimeUnit.MILLISECONDS);
+            ChatRoom chat3 = chat3Promise.get(500, TimeUnit.MILLISECONDS);
 
-        c1.degradePerformance("id3");
+            c1.degradePerformance("id3");
 
-        c1.sendMessage("TEST", chat1, 0);
+            c1.sendMessage("TEST", chat1, 0);
 
-        Message m1 = msg1Promise.get(500, TimeUnit.MILLISECONDS);
-        Message m2 = msg2Promise.get(500, TimeUnit.MILLISECONDS);
+            Message m1 = msg1Promise.get(500, TimeUnit.MILLISECONDS);
+            Message m2 = msg2Promise.get(500, TimeUnit.MILLISECONDS);
 
-        Thread.sleep(500);
-        assertTrue(msg3List.isEmpty());
+            Thread.sleep(500);
+            assertTrue(msg3List.isEmpty());
 
-        assertEquals("TEST", m1.msg());
-        assertEquals("id1", m1.sender());
+            assertEquals("TEST", m1.msg());
+            assertEquals("id1", m1.sender());
 
-        assertEquals("TEST", m2.msg());
-        assertEquals("id1", m2.sender());
+            assertEquals("TEST", m2.msg());
+            assertEquals("id1", m2.sender());
 
-        c2.sendMessage("TEST2", chat2, 0);
+            c2.sendMessage("TEST2", chat2, 0);
 
-        for (int i = 0; i < 50; i++) {
-            Thread.sleep(10);
-            if (1 == chat3.getWaiting().size())
-                break;
+            for (int i = 0; i < 50; i++) {
+                Thread.sleep(10);
+                if (1 == chat3.getWaiting().size())
+                    break;
+            }
+            assertTrue(msg3List.isEmpty());
+            assertEquals(1, chat3.getWaiting().size());
+            assertEquals("TEST2", chat3.getWaiting().stream().findFirst().orElseThrow().msg());
+
+            assertTrue(msg3.await(2000, TimeUnit.MILLISECONDS));
+
+            assertEquals("TEST", msg3List.get(0).msg());
+            assertEquals("TEST2", msg3List.get(1).msg());
         }
-        assertTrue(msg3List.isEmpty());
-        assertEquals(1, chat3.getWaiting().size());
-        assertEquals("TEST2", chat3.getWaiting().stream().findFirst().orElseThrow().msg());
-
-        assertTrue(msg3.await(2000, TimeUnit.MILLISECONDS));
-
-        assertEquals("TEST", msg3List.get(0).msg());
-        assertEquals("TEST2", msg3List.get(1).msg());
-
-        c1.close();
-        c2.close();
-        c3.close();
     }
 
     @Test
@@ -210,84 +208,82 @@ class ChatTest {
         CompletableFuture<String> disc1Promise = new CompletableFuture<>();
         CompletableFuture<String> disc3Promise = new CompletableFuture<>();
         CountDownLatch disc2 = new CountDownLatch(2);
+        try (
+                Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> {
+                            if (e.getPropertyName().equals("USER_CONNECTED"))
+                                users1.countDown();
+                            else
+                                disc1Promise.complete((String) e.getOldValue());
+                        },
+                        e -> msg1Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
 
-        Peer c1 = new Peer("id1", 12345, e -> chat1Promise.complete((ChatRoom) e.getNewValue()),
-                e -> {
-                    if (e.getPropertyName().equals("USER_CONNECTED"))
-                        users1.countDown();
-                    else
-                        disc1Promise.complete((String) e.getOldValue());
-                },
-                e -> msg1Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
+                Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> {
+                            if (e.getPropertyName().equals("USER_CONNECTED"))
+                                users2.countDown();
+                            else
+                                disc2.countDown();
+                        },
+                        e -> msg2Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
 
-        Peer c2 = new Peer("id2", 12346, e -> chat2Promise.complete((ChatRoom) e.getNewValue()),
-                e -> {
-                    if (e.getPropertyName().equals("USER_CONNECTED"))
-                        users2.countDown();
-                    else
-                        disc2.countDown();
-                },
-                e -> msg2Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
+                Peer c3 = new Peer("id3", 12347, e -> chat3Promise.complete((ChatRoom) e.getNewValue()),
+                        e -> {
+                            if (e.getPropertyName().equals("USER_CONNECTED"))
+                                users3.countDown();
+                            else
+                                disc3Promise.complete((String) e.getOldValue());
+                        },
+                        e -> msg3Promise.complete(((MessageGUI) e.getNewValue()).message()), true)
+        ) {
+            assertTrue(users1.await(500, TimeUnit.MILLISECONDS));
+            assertTrue(users2.await(500, TimeUnit.MILLISECONDS));
+            assertTrue(users3.await(500, TimeUnit.MILLISECONDS));
 
-        Peer c3 = new Peer("id3", 12347, e -> chat3Promise.complete((ChatRoom) e.getNewValue()),
-                e -> {
-                    if (e.getPropertyName().equals("USER_CONNECTED"))
-                        users3.countDown();
-                    else
-                        disc3Promise.complete((String) e.getOldValue());
-                },
-                e -> msg3Promise.complete(((MessageGUI) e.getNewValue()).message()), true);
-        assertTrue(users1.await(500, TimeUnit.MILLISECONDS));
-        assertTrue(users2.await(500, TimeUnit.MILLISECONDS));
-        assertTrue(users3.await(500, TimeUnit.MILLISECONDS));
+            Set<String> users = Set.of("id1", "id2", "id3");
+            c1.createRoom("room", users);
 
-        Set<String> users = Set.of("id1", "id2", "id3");
-        c1.createRoom("room", users);
+            ChatRoom chat1 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
+            ChatRoom chat2 = chat2Promise.get(500, TimeUnit.MILLISECONDS);
+            ChatRoom chat3 = chat3Promise.get(500, TimeUnit.MILLISECONDS);
 
-        ChatRoom chat1 = chat1Promise.get(500, TimeUnit.MILLISECONDS);
-        ChatRoom chat2 = chat2Promise.get(500, TimeUnit.MILLISECONDS);
-        ChatRoom chat3 = chat3Promise.get(500, TimeUnit.MILLISECONDS);
+            c2.disconnect();
 
-        c2.disconnect();
+            assertTrue(disc2.await(500, TimeUnit.MILLISECONDS));
+            assertEquals("id2", disc1Promise.get(500, TimeUnit.MILLISECONDS));
+            assertEquals("id2", disc3Promise.get(500, TimeUnit.MILLISECONDS));
 
-        assertTrue(disc2.await(500, TimeUnit.MILLISECONDS));
-        assertEquals("id2", disc1Promise.get(500, TimeUnit.MILLISECONDS));
-        assertEquals("id2", disc3Promise.get(500, TimeUnit.MILLISECONDS));
+            c1.sendMessage("TEST", chat1, 0);
 
-        c1.sendMessage("TEST", chat1, 0);
+            Message m1 = msg1Promise.get(500, TimeUnit.MILLISECONDS);
+            Message m3 = msg3Promise.get(500, TimeUnit.MILLISECONDS);
+            assertThrows(TimeoutException.class, () -> msg2Promise.get(500, TimeUnit.MILLISECONDS));
 
-        Message m1 = msg1Promise.get(500, TimeUnit.MILLISECONDS);
-        Message m3 = msg3Promise.get(500, TimeUnit.MILLISECONDS);
-        assertThrows(TimeoutException.class, () -> msg2Promise.get(500, TimeUnit.MILLISECONDS));
+            assertEquals("TEST", m1.msg());
+            assertEquals("id1", m1.sender());
 
-        assertEquals("TEST", m1.msg());
-        assertEquals("id1", m1.sender());
+            assertEquals("TEST", m3.msg());
+            assertEquals("id1", m3.sender());
 
-        assertEquals("TEST", m3.msg());
-        assertEquals("id1", m3.sender());
+            assertNotNull(c1.getDiscMsg("id2"));
+            assertEquals(1, c1.getDiscMsg("id2").size());
+            var msg = ((MessagePacket) c1.getDiscMsg("id2").stream().findFirst().orElseThrow()).msg();
+            assertEquals("TEST", msg.msg());
+            assertEquals("id1", msg.sender());
 
-        assertNotNull(c1.getDiscMsg("id2"));
-        assertEquals(1, c1.getDiscMsg("id2").size());
-        var msg = ((MessagePacket) c1.getDiscMsg("id2").stream().findFirst().orElseThrow()).msg();
-        assertEquals("TEST", msg.msg());
-        assertEquals("id1", msg.sender());
+            c2.start();
 
-        c2.start();
+            Message m2 = msg2Promise.get(500, TimeUnit.MILLISECONDS);
+            assertEquals("TEST", m2.msg());
+            assertEquals("id1", m2.sender());
 
-        Message m2 = msg2Promise.get(500, TimeUnit.MILLISECONDS);
-        assertEquals("TEST", m2.msg());
-        assertEquals("id1", m2.sender());
-
-        for (int i = 0; i < 50; i++) {
-            if (c1.getDiscMsg("id2") == null || c1.getDiscMsg("id2").isEmpty())
-                break;
-            Thread.sleep(10);
+            for (int i = 0; i < 50; i++) {
+                if (c1.getDiscMsg("id2") == null || c1.getDiscMsg("id2").isEmpty())
+                    break;
+                Thread.sleep(10);
+            }
+            assertTrue(c1.getDiscMsg("id2") == null || c1.getDiscMsg("id2").isEmpty());
         }
-        assertTrue(c1.getDiscMsg("id2") == null || c1.getDiscMsg("id2").isEmpty());
-
-        c1.close();
-        c2.close();
-        c3.close();
     }
 
 }
