72b38bdf816188f13e65397a38a460ca722c5750
jackorse <giacomo.orsenigo@gmail.com>
2024-03-04 17:50:25 +0100

    Remove workaround for GitHub action
    The action is running with only 1 core, so if we use synchronized, the entire application will block.
    Fixed in f0369cfd

--- main/src/main/java/org/HACO/Peer.java
+++ main/src/main/java/org/HACO/Peer.java
@@ -257,17 +257,10 @@ public class Peer implements Closeable {
         ids.forEach(id -> {
             if (!id.equals(this.id)) {
                 if (sockets.containsKey(id)) {
-                    //Apparently the GitHub action breaks if we concurrently write on different sockets :/
-                    //To be investigated.
-                    if (testing) {
+                    executorService.execute(() -> {
                         System.out.println("[" + this.id + "] sending " + packet + " to " + id);
                         sendSinglePeer(packet, id);
-                    } else {
-                        executorService.execute(() -> {
-                            System.out.println("[" + this.id + "] sending " + packet + " to " + id);
-                            sendSinglePeer(packet, id);
-                        });
-                    }
+                    });
                 } else {
                     System.out.println("[" + this.id + "] Peer " + id + " currently disconnected, enqueuing packet only for him...");
                     disconnectMsgs.computeIfAbsent(id, k -> ConcurrentHashMap.newKeySet());
