31c47db7d8b442d65017be23312f2c3b8cec29b1
Francesco Spangaro <francescospangaro@gmail.com>
2024-03-13 19:19:11 +0100

    Pretty toString Closes #2

--- main/src/main/java/org/HACO/utility/Message.java
+++ main/src/main/java/org/HACO/utility/Message.java
@@ -2,6 +2,16 @@ package org.HACO.utility;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record Message(String msg, Map<String, Integer> vectorClocks, String sender) implements Serializable {
+    @Override
+    public String toString(){
+        return STR."""
+                [\{this.sender}]: \{this.msg} \t
+                \{this.vectorClocks.keySet().stream()
+                .map((k) -> STR."   User:\{k} PID: \{this.vectorClocks.get(k)}")
+                .collect(Collectors.joining(",\n", "Vector clocks: {\n", "}\n"))}
+                """;
+    }
 }
