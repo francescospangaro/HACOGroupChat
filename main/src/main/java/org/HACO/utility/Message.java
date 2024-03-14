ea3ebbd6323021880547b74a663ecde7cb8097d3
Francesco Spangaro <francescospangaro@gmail.com>
2024-03-14 12:00:00 +0100

    PRETTIER toString

--- main/src/main/java/org/HACO/utility/Message.java
+++ main/src/main/java/org/HACO/utility/Message.java
@@ -5,13 +5,15 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public record Message(String msg, Map<String, Integer> vectorClocks, String sender) implements Serializable {
+    //Apparently in JList to format text you have to use HTML
     @Override
     public String toString(){
         return STR."""
-                [\{this.sender}]: \{this.msg} \t
+        <html>
+                [\{this.sender}]: \{this.msg} <br>
                 \{this.vectorClocks.keySet().stream()
-                .map((k) -> STR."   User:\{k} PID: \{this.vectorClocks.get(k)}")
-                .collect(Collectors.joining(",\n", "Vector clocks: {\n", "}\n"))}
+                .map((k) -> STR."<pre>   User:\{k} PID: \{this.vectorClocks.get(k)}</pre>")
+                .collect(Collectors.joining(",", "Vector clocks: {<br>", "}"))}
                 """;
     }
 }
