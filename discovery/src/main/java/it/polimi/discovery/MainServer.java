16d67c8dec9a5804c99eced8d0986cea9e8f64c8
Francesco Spangaro <francescospangaro@gmail.com>
2024-07-04 12:52:01 +0200

    Added try catch

--- discovery/src/main/java/it/polimi/discovery/MainServer.java
+++ discovery/src/main/java/it/polimi/discovery/MainServer.java
@@ -6,13 +6,13 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
  * Main class which starts the discovery
  *
  * @implNote it is important that this class does not declare any static logger fields,
- * as it might trigger log4j initialization before {@link #main(String[])} has the
+ * as it might trigger log4j initialization before {@link #main()} has the
  * possibility to set the {@code log4j.configurationFile} system property to the
  * correct file
  */
 public class MainServer {
 
-    public static void main(String[] args) {
+    public static void main() {
         // Configure log4j file if none is already set
         if (System.getProperty("log4j.configurationFile") == null)
             System.setProperty("log4j.configurationFile", "log4j2-discovery.xml");
@@ -22,7 +22,8 @@ public class MainServer {
         SLF4JBridgeHandler.install();
 
 
-        DiscoveryServer s = new DiscoveryServer();
-        s.start();
+        try (DiscoveryServer s = new DiscoveryServer()) {
+            s.start();
+        }
     }
 }
