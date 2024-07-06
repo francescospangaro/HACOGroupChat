0af287ddaa5bf2c227cd6c09eb1a5920197bb208
Francesco Spangaro <francescospangaro@gmail.com>
2024-07-06 18:52:17 +0200

    Added new lines to messages, limiting the amount of letters shown

--- common/src/main/java/it/polimi/messages/StringMessage.java
+++ common/src/main/java/it/polimi/messages/StringMessage.java
@@ -1,7 +1,6 @@
 package it.polimi.messages;
 
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * String message object
@@ -13,6 +12,16 @@ import java.util.stream.Collectors;
 public record StringMessage(String msg, Map<String, Integer> vectorClocks, String sender) implements Message {
     @Override
     public String toString() {
-        return msg;
+        StringBuilder temp = new StringBuilder();
+        for (int i = 0; i < msg.length(); i += 30) {
+            int min = Math.min(i + 30, msg.length());
+            temp.append(msg, i, min).append('\n');
+            if ((min != msg.length()) &&
+                (msg.charAt(i + 30) != ' ') &&
+                (msg.charAt(i + 29) != ' ')) {
+                temp.append('-');
+            }
+        }
+        return temp.toString();
     }
 }
